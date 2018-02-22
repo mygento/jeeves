56a2e8b20307717fa596705b32f6a121faf6853c
nikita tarasov <nikita@mygento.ru>
2018-02-22 11:23:29 +0300

    workplace project structure

--- src/Jeeves/Console/Command/Workplace.php
+++ src/Jeeves/Console/Command/Workplace.php
@@ -16,7 +16,8 @@ class Workplace extends BaseCommand
             ->setAliases(array('workplace'))
             ->setDescription('Create workplace')
             ->setDefinition(array(
-                new InputArgument('name', InputArgument::REQUIRED, 'Name of the entity'),
+                new InputArgument('name', InputArgument::OPTIONAL, 'Name of the entity'),
+                new InputArgument('repo', InputArgument::OPTIONAL, 'Project repository url'),
               ))
             ->setHelp(<<<EOT
 <info>php jeeves.phar new-workplace</info>
@@ -31,7 +32,7 @@ EOT
         // Clone to a non-bare repository
         $folder = strtolower($input->getArgument('name'));
         $folder = $io->askAndValidate(
-            'Folder [<comment>'.$folder.'</comment>]: ',
+            'Project Name [<comment>'.$folder.'</comment>]: ',
             function ($value) use ($folder) {
                 if (null === $value) {
                     return $folder;
@@ -41,6 +42,58 @@ EOT
             null,
             $folder
         );
-        $repository = \Gitonomy\Git\Admin::cloneTo($folder, 'https://github.com/mygento/workplace.git', false);
+        $repo = strtolower($input->getArgument('repo'));
+        $repo = $io->askAndValidate(
+            'Project repository [<comment>'.$repo.'</comment>]: ',
+            function ($value) use ($repo) {
+                if (null === $value) {
+                    return $repo;
+                }
+                return $value;
+            },
+            null,
+            $repo
+        );
+        if (!is_dir($folder.'-project')) {
+            mkdir($folder.'-project');
+        }
+        $workplaceFolder = $folder.'-project'.DIRECTORY_SEPARATOR.$folder.'-workplace';
+        try {
+            $io->write(sprintf("Cloning: <info>%s</info>.", 'workplace'));
+            $repository = \Gitonomy\Git\Admin::cloneTo(
+                $workplaceFolder,
+                'https://github.com/mygento/workplace.git',
+                false
+            );
+        } catch (\Gitonomy\Git\Exception\RuntimeException $e) {
+            $io->writeError($e->getMessage());
+        }
+        try {
+            $io->write(sprintf("Cloning: <info>%s</info>.", $repo));
+            $repository = \Gitonomy\Git\Admin::cloneTo(
+                $folder.'-project'.DIRECTORY_SEPARATOR.$folder,
+                $repo,
+                false
+            );
+        } catch (\Gitonomy\Git\Exception\RuntimeException $e) {
+            $io->writeError($e->getMessage());
+        }
+        $srcFolder = $workplaceFolder.DIRECTORY_SEPARATOR.'src';
+        if (!is_dir($srcFolder)) {
+            $io->write(sprintf("Creating symlink to: <info>%s</info>.", $folder));
+            symlink('../'.$folder, $srcFolder);
+            return;
+        }
+        if (!is_link($srcFolder) &&
+            (
+                !(new \FilesystemIterator($srcFolder))->valid() ||
+                count(scandir($srcFolder)) <= 3
+            )
+        ) {
+            $io->write(sprintf("Creating symlink to: <info>%s</info>.", $folder));
+            unlink($srcFolder.DIRECTORY_SEPARATOR.'.keep');
+            rmdir($srcFolder);
+            symlink('../'.$folder, $srcFolder);
+        }
     }
 }
