4b73671c26a45f30c004ec63cfd2c090ffa0ef65
nikita tarasov <nikita@mygento.ru>
2019-02-11 15:01:27 +0300

    fix crud listing component

--- test/Expectations/Crud/Model/ResourceModel/Banner/Collection.php
+++ test/Expectations/Crud/Model/ResourceModel/Banner/Collection.php
@@ -13,8 +13,8 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
     protected function _construct()
     {
         $this->_init(
-           \Mygento\SampleModule\Model\Banner::class,
-           \Mygento\SampleModule\Model\ResourceModel\Banner::class
+            \Mygento\SampleModule\Model\Banner::class,
+            \Mygento\SampleModule\Model\ResourceModel\Banner::class
         );
     }
 }
