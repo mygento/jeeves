3da4996038c4dfd63af3d47d46b4c6ad7e1976ae
nikita tarasov <nikita@mygento.ru>
2021-12-14 15:42:54 +0300

    [WIP v1] CS rules

--- src/Jeeves/Console/Command/BaseCommand.php
+++ src/Jeeves/Console/Command/BaseCommand.php
@@ -2,6 +2,7 @@
 
 namespace Mygento\Jeeves\Console\Command;
 
+use Mygento\CS\Config\Module;
 use Mygento\Jeeves\Console\Application;
 use Mygento\Jeeves\IO\IOInterface;
 use Mygento\Jeeves\IO\NullIO;
@@ -70,38 +71,8 @@ abstract class BaseCommand extends Command
             ->name('*.php')
             ->in(\Mygento\Jeeves\Console\Application::GEN);
 
-        $config = \PhpCsFixer\Config::create()
-            ->setRules([
-                '@PSR2' => true,
-                'array_syntax' => ['syntax' => 'short'],
-                'concat_space' => ['spacing' => 'one'],
-                'include' => true,
-                'new_with_braces' => true,
-                'no_empty_statement' => true,
-                'no_extra_consecutive_blank_lines' => true,
-                'no_leading_import_slash' => true,
-                'no_leading_namespace_whitespace' => true,
-                'no_multiline_whitespace_around_double_arrow' => true,
-                'no_multiline_whitespace_before_semicolons' => true,
-                'no_singleline_whitespace_before_semicolons' => true,
-                'no_trailing_comma_in_singleline_array' => true,
-                'no_unused_imports' => true,
-                'no_whitespace_in_blank_line' => true,
-                'object_operator_without_whitespace' => true,
-                'ordered_imports' => true,
-                'standardize_not_equals' => true,
-                'ternary_operator_spaces' => true,
-                // mygento
-                'phpdoc_order' => true,
-                'phpdoc_types' => true,
-                'phpdoc_add_missing_param_annotation' => true,
-                'single_quote' => true,
-                'ternary_to_null_coalescing' => true,
-                'lowercase_cast' => true,
-                'no_empty_comment' => true,
-                'no_empty_phpdoc' => true,
-                'return_type_declaration' => true,
-            ])->setFinder($finder);
+        $config = new Module();
+        $config->setFinder($finder);
 
         $resolver = new \PhpCsFixer\Console\ConfigurationResolver($config, [], getcwd(), new \PhpCsFixer\ToolInfo());
         $errorsManager = new \PhpCsFixer\Error\ErrorsManager();
